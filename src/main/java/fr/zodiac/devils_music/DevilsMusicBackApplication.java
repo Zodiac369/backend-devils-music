b852f42a6faebe620944e16ee043f262fdf150cd
Zodiac369 <emre.ogk@hotmail.fr>
2024-09-04 02:42:54 +0200

    first commit

---
+++ src/main/java/fr/zodiac/devils_music/DevilsMusicBackApplication.java
@@ -0,0 +1,13 @@
+package fr.zodiac.devils_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevilsMusicBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevilsMusicBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/zodiac/devils_music/DevilsMusicBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.zodiac.devils_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevilsMusicBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
